d1b5a0173a84472cdfd958888765a7ea7ba2bd6e
vimark <stanislava1804@gmail.com>
2024-05-10 15:48:19 +0200

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
+from django.template.defaulttags import lorem
 
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'lorem ipsum'
+    }
+
+    return render(request, 'main/about.html', context)
