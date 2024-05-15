29511e22f85c759209f4cc6926ed5b6da9374d6a
vimark <stanislava1804@gmail.com>
2024-05-15 10:29:32 +0200

    catalog

--- main/views.py
+++ main/views.py
@@ -7,12 +7,10 @@ from goods.models import Categories
 
 # Create your views here.
 def index(request):
-    categories = Categories.objects.all()
 
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
