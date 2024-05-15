f1bb09fd2ed4ebfaeb62e3cf0ab58c0cf24e4b1f
vimark <stanislava1804@gmail.com>
2024-05-15 11:27:09 +0200

    filters

--- goods/views.py
+++ goods/views.py
@@ -2,14 +2,34 @@ from django.shortcuts import render
 
 from goods.models import Products
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+        
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     context = {
         'title': 'Home - каталог',
         'goods': goods,
+        'slug_url': category_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'goods/product.html', context=context)
